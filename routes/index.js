d9d4a5d37ece79827eabb4fbfd21cf68eb6eca31
Asad <asadkhan2168735@gmail.com>
2023-10-26 07:44:34 -0700

    ..

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.use('/property', require('./property'));
 router.use('/lov', require('./lov'));
 router.use('/users', require('./users'));
+// router.use('/property', require('./property'));
 
 module.exports = router;
