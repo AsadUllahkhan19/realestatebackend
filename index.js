7474fd1cff854a128df8700693dca6b7b28a683e
Asad <asadkhan2168735@gmail.com>
2024-01-26 04:13:39 -0800

    ..

--- index.js
+++ index.js
@@ -9,59 +9,59 @@ require("dotenv").config();
 // const swaggerUi = require('swagger-ui-express');
 const main = require("./config/db");
 
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
 // const UserMiddleWare = require("./middlewares/UserVerify");
 // const helpers = require("./helpers/validation");
-const options = {
-  definition: {
-    openapi: "3.1.0",
-    info: {
-      title: "LogRocket Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "LogRocket",
-        url: "https://logrocket.com",
-        email: "info@email.com",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:4000",
-        url: "https://realestatebackend-woad.vercel.app"
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-const specs = swaggerJsdoc(options);
+// const options = {
+//   definition: {
+//     openapi: "3.1.0",
+//     info: {
+//       title: "LogRocket Express API with Swagger",
+//       version: "0.1.0",
+//       description:
+//         "This is a simple CRUD API application made with Express and documented with Swagger",
+//       license: {
+//         name: "MIT",
+//         url: "https://spdx.org/licenses/MIT.html",
+//       },
+//       contact: {
+//         name: "LogRocket",
+//         url: "https://logrocket.com",
+//         email: "info@email.com",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:4000",
+//         url: "https://realestatebackend-woad.vercel.app"
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"],
+// };
+// const specs = swaggerJsdoc(options);
 // Your Twilio Account SID and Auth Token
 
 app.use(cors('*'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs,{ explorer: true })
-);
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(specs,{ explorer: true })
+// );
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Fine");
 });
 
-app.use(
-  "/static",
-  express.static(path.join(__dirname, "files", "data", "uploads"))
-);
+// app.use(
+//   "/static",
+//   express.static(path.join(__dirname, "files", "data", "uploads"))
+// );
 
 main();
 
